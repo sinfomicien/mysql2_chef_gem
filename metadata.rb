da6bbef0096a03547785bb1752db9818eaba4176
Tim Smith <tsmith84@gmail.com>
2016-04-25 09:53:31 -0700

    Add additional metadata to resolve FC warnings

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer 'Nicolas Blanc'
 maintainer_email 'sinfomicien@gmail.com'
 license 'Apache 2.0'
 description 'Provides the mysql2_chef_gem resource'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.2'
 
 supports 'amazon'
@@ -16,3 +17,6 @@ supports 'ubuntu'
 depends 'build-essential'
 depends 'mysql', '>= 6.0'
 depends 'mariadb'
+
+source_url 'https://github.com/sinfomicien/mysql2_chef_gem' if respond_to?(:source_url)
+issues_url 'https://github.com/sinfomicien/mysql2_chef_gem/issues' if respond_to?(:issues_url)
