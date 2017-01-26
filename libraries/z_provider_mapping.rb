459a21f04848085d42412c5dceb552069da015cb
Tim Smith <tsmith@chef.io>
2017-01-26 11:33:15 -0800

    Remove support for EOL Fedora
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/z_provider_mapping.rb
+++ libraries/z_provider_mapping.rb
@@ -5,8 +5,7 @@ Chef::Platform.set platform: :amazon, resource: :mysql2_chef_gem, provider: Chef
 Chef::Platform.set platform: :centos, version: '< 7.0', resource: :mysql2_chef_gem, provider: Chef::Provider::Mysql2ChefGem::Mysql
 Chef::Platform.set platform: :centos, version: '>= 7.0', resource: :mysql2_chef_gem, provider: Chef::Provider::Mysql2ChefGem::Mysql
 Chef::Platform.set platform: :debian, resource: :mysql2_chef_gem, provider: Chef::Provider::Mysql2ChefGem::Mysql
-Chef::Platform.set platform: :fedora, version: '< 19', resource: :mysql2_chef_gem, provider: Chef::Provider::Mysql2ChefGem::Mysql
-Chef::Platform.set platform: :fedora, version: '>= 19', resource: :mysql2_chef_gem, provider: Chef::Provider::Mysql2ChefGem::Mysql
+Chef::Platform.set platform: :fedora, resource: :mysql2_chef_gem, provider: Chef::Provider::Mysql2ChefGem::Mysql
 Chef::Platform.set platform: :omnios, resource: :mysql2_chef_gem, provider: Chef::Provider::Mysql2ChefGem::Mysql
 Chef::Platform.set platform: :redhat, version: '< 7.0', resource: :mysql2_chef_gem, provider: Chef::Provider::Mysql2ChefGem::Mysql
 Chef::Platform.set platform: :redhat, version: '>= 7.0', resource: :mysql2_chef_gem, provider: Chef::Provider::Mysql2ChefGem::Mysql
