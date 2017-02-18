6aa9641eba0a94186c7bf59e65eb93e6a7edd28d
Noel Georgi <noel.georgi@reancloud.com>
2017-02-19 00:57:48 +0530

    Fixed local conflict issue

--- libraries/z_provider_mapping.rb
+++ libraries/z_provider_mapping.rb
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-# #########
 # # mysql2_chef_gem
 # #########
 class Chef::Provider::Mysql2ChefGem::Mysql
   provides :mysql2_chef_gem, platform: 'debian'
   provides :mysql2_chef_gem, platform: 'ubuntu'
-=======
-#########
-# mysql2_chef_gem
-#########
-class Chef::Provider::Mysql
-  context :mysql2_chef_gem, platform: 'debian,', platform_version: '>= 7'
-  context :mysql2_chef_gem, platform: 'ubuntu,', platform_version: '>= 12.04'
->>>>>>> 0c22ba843253a962b25eca6f127d84c3c9917247
 end
