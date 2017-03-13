440f53bef5be9643b3419e9120b3bf8ada02620b
Tim Smith <tsmith@chef.io>
2017-03-12 18:46:33 -0700

    Test kitchen config improvements
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/mysql2_chef_gem_test/recipes/default.rb
+++ test/fixtures/cookbooks/mysql2_chef_gem_test/recipes/default.rb
@@ -1,5 +1,14 @@
 apt_update 'update'
 
+case node['mysql2_chef_gem']['package_version']
+when '5.5'
+  include_recipe 'yum-mysql-community::mysql55'
+when '5.6'
+  include_recipe 'yum-mysql-community::mysql56'
+when '5.7'
+  include_recipe 'yum-mysql-community::mysql57'
+end
+
 mysql2_chef_gem 'default' do
   package_version node['mysql2_chef_gem']['package_version'] if node['mysql2_chef_gem']
   provider Chef::Provider::Mysql2ChefGem::Mysql if node['mysql2_chef_gem']['provider'] == 'mysql'
