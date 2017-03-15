910e339af7371d2c210a5d8f443ce188fec43f9f
Tim Smith <tsmith@chef.io>
2017-03-15 13:17:15 -0700

    Update tests for the new resource format
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/mysql2_chef_gem_test/recipes/default.rb
+++ test/fixtures/cookbooks/mysql2_chef_gem_test/recipes/default.rb
@@ -9,9 +9,14 @@ when '5.7'
   include_recipe 'yum-mysql-community::mysql57'
 end
 
-mysql2_chef_gem 'default' do
-  package_version node['mysql2_chef_gem']['package_version'] if node['mysql2_chef_gem']
-  provider Chef::Provider::Mysql2ChefGem::Mysql if node['mysql2_chef_gem']['provider'] == 'mysql'
-  provider Chef::Provider::Mysql2ChefGem::Mariadb if node['mysql2_chef_gem']['provider'] == 'mariadb'
-  action :install
+if node['mysql2_chef_gem']['provider'] == 'mysql'
+  mysql2_chef_gem 'default' do
+    package_version node['mysql2_chef_gem']['package_version'] if node['mysql2_chef_gem']
+    action :install
+  end
+else
+  mysql2_chef_gem_mariadb 'default' do
+    package_version node['mysql2_chef_gem']['package_version'] if node['mysql2_chef_gem']
+    action :install
+  end
 end
