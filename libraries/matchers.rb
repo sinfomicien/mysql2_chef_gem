fa5fb75d8b588286107bdae109356cb1952176c7
Marc Cirauqui <marc.cirauqui@abiquo.com>
2017-10-11 21:14:43 +0200

    Add matcher for mysql2_chef_gem_mariadb
    Adds a matcher for the `mysql2_chef_gem_mariadb` resource.

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -2,4 +2,8 @@ if defined?(ChefSpec)
   def install_mysql2_chef_gem(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:mysql2_chef_gem, :install, resource_name)
   end
+
+  def install_mysql2_chef_gem_mariadb(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:mysql2_chef_gem_mariadb, :install, resource_name)
+  end
 end
